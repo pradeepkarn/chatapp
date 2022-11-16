3c348ff12b5ea83472a5513d4a9d2f5398261c24
Pradeep Karn <mail2pkarn@gmail.com>
2022-11-16 13:48:54 +0530

    cahnages

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
-    HOST: "localhost",
+    HOST: "199.192.24.175",
     USER: "node_learn_user",
     PASSWORD: "NODE9572",
     DB: "node_learn",
