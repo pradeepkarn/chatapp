7aaeb25d6cb809e0c363f89217f5457fcf21af5b
Pradeep Karn <mail2pkarn@gmail.com>
2022-11-16 13:45:03 +0530

    changes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
-    HOST: "199.192.24.175",
+    HOST: "localhost",
     USER: "node_learn_user",
     PASSWORD: "NODE9572",
     DB: "node_learn",
