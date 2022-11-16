b84edbdaeaf7227f376a879f99870152ac66f32d
Pradeep Karn <mail2pkarn@gmail.com>
2022-11-16 13:52:06 +0530

    rootdab

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,9 @@
 const dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
-    HOST: "199.192.24.175",
-    USER: "node_learn_user",
-    PASSWORD: "NODE9572",
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
     DB: "node_learn",
     dialect: 'mysql',
     pool: {
