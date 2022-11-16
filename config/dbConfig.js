84394ebfb96ad2a54e404db5572fc0373e02b433
Pradeep Karn <mail2pkarn@gmail.com>
2022-11-16 13:57:52 +0530

    loacahosty

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,20 @@
 const dotenv = require("dotenv");
 dotenv.config();
+// module.exports = {
+//     HOST: "199.192.24.175",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "node_learn",
+//     dialect: 'mysql',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// }
 module.exports = {
-    HOST: "199.192.24.175",
+    HOST: "localhost",
     USER: "root",
     PASSWORD: "",
     DB: "node_learn",
