7462e98ff2706bfbcf9c17fc84bbde87e872a448
Pradeep Karn <mail2pkarn@gmail.com>
2022-11-16 15:14:53 +0530

    new conf db

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,20 @@
 const dotenv = require("dotenv");
 dotenv.config();
+module.exports = {
+    HOST: "199.192.24.175",
+    USER: "node_learn_user",
+    PASSWORD: "NODE9572",
+    DB: "node_learn",
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+}
 // module.exports = {
-//     HOST: "199.192.24.175",
+//     HOST: "localhost",
 //     USER: "root",
 //     PASSWORD: "",
 //     DB: "node_learn",
@@ -13,16 +26,3 @@ dotenv.config();
 //         idle: 10000
 //     }
 // }
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "node_learn",
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-}
