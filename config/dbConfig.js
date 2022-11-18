e6a9caa1cbfb928a7c0a3d18a3cbc80045e3b084
Pradeep Karn <mail2pkarn@gmail.com>
2022-11-18 18:57:57 +0530

    push to server

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -14,16 +14,16 @@ module.exports = {
         idle: 10000
     }
 }
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "node_learn",
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-}
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "node_learn",
+//     dialect: 'mysql',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// }
