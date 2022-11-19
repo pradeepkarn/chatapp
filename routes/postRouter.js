c309d80c0f01b09c9a471993970bc518e6483379
Pradeep Karn <mail2pkarn@gmail.com>
2022-11-19 10:59:13 +0530

    posts router

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,4 +1,6 @@
 const postController = require("../controllers/postController.js");
 const router = require('express').Router()
 router.post("/add-post",postController.addPost);
+router.get("/get/:id",postController.getPost);
+router.get("/get",postController.getAllPost);
 module.exports = router
