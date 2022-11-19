e643124c2250b88384bb8eaa1b5123e1bf83acab
Pradeep Karn <mail2pkarn@gmail.com>
2022-11-19 11:04:50 +0530

    post

--- controllers/postController.js
+++ controllers/postController.js
@@ -11,6 +11,7 @@ const addPost = async (req, res)=>{
             last_name: req.body.first_name,
             creator_image: req.body.creator_image,
             image : req.body.image,
+            comments: req.body.comments,
             active : true,
         });
         const data = {status:true,msg:"Post created",data:post}
@@ -34,6 +35,38 @@ const getPost = async (req,res)=>{
     }
     
 }
+
+// const addCoomentOnPost = async (req, res)=>{
+//     const signUpData = {
+//         first_name: req.body.first_name,
+//         last_name: req.body.last_name,
+//         mobile : req.body.mobile,
+//         password : req.body.password,
+//         token: null,
+//         active: 1
+//     }
+//     if (req.body.mobile=="") {
+//         const data = {status:false,msg:"Empty mobile number is not allowed",data:null}
+//         res.status(200).json(data)
+//         return;
+//     }
+//     let userExist = await User.findOne({where : {mobile:req.body.mobile}})
+//     if (userExist) {
+//         const data = {status:false,msg:"User already registered",data:null}
+//         res.status(200).json(data)
+//         return;
+//     }else{
+//         const user = await User.create(signUpData);
+//         if (user) {
+//             const data = {status:true,msg:"Signup success",data:signUpData}
+//             res.status(200).json(data)
+//         }else{
+//             const data = {status:false,msg:"Signup failed",data:null}
+//             res.status(200).json(data)
+//         }
+//         return;
+//     }
+// }
 
 const getAllPost = async (req,res)=>{
     //for all data
