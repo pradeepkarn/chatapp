ae62e160dd85d999c2cd3d7a27356f31ad94da50
Pradeep Karn <mail2pkarn@gmail.com>
2022-11-23 18:49:20 +0530

    sdds

---
+++ passport-config.js
@@ -0,0 +1,27 @@
+const LocalStartegy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+function initialize(passport, getUser) {
+    const authenticateUser = (mobile, password, done) => {
+        const user = getUser(mobile)
+        if (user==null) {
+            return done(null, false, { message: 'No user with that mobile' })
+        }
+
+        try {
+            if ( bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            }else{
+                return done(null, false, { message: 'Password incorrect' })
+            }
+        } catch (e) {
+            return done(e)
+        }
+
+    }
+    passport.use(new LocalStartegy({ usernameField: 'mobile' }),
+    authenticateUser)
+    passport.serializeUser((user, done) => { })
+    passport.deserializeUser((id, done) => { })
+}
+
+module.exports = initialize
